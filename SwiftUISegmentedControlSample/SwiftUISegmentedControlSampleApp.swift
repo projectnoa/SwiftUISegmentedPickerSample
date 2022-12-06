840ac430a8e15ff7e8461cee5834b569db161d50
Juan <projectnoa@users.noreply.github.com>
2022-12-06 14:57:06 -0700

    Initial Commit

---
+++ SwiftUISegmentedControlSample/SwiftUISegmentedControlSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//	SwiftUISegmentedControlSampleApp.swift
+//  SwiftUISegmentedControlSample
+//
+//  Created by Juan Mueller on 12/6/22.
+//  For more, visit www.ajourneyforwisdom.com
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISegmentedControlSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
